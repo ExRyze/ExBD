4a78d1ee80e0c7e368c20250e2cf3d44c804ae69
ExRyze <vaisyagovinandas@gmail.com>
2022-12-01 09:25:31 +0800

    Testing - Middleware

--- app/core/Middleware.php
+++ app/core/Middleware.php
@@ -4,16 +4,17 @@ class Middleware {
 
   public static function auth($status = true) {
     if($status === true && !isset($_SESSION['user'])) {
-      header('location: '.BASE_URL.'/login');
+      return header('location: '.BASE_URL.'/login');
     } else if($status === false && isset($_SESSION['user'])) {
-      header('location: '.BASE_URL);
+      return header('location: '.BASE_URL);
     }
   }
 
   public static function role($role) {
-    Middleware::auth();
-    if($_SESSION['user']['role'] != $role) {
-      header('location: '.BASE_URL);
+    if(!isset($_SESSION['user'])) { 
+      if($_SESSION['user']['role'] != $role) {
+        return header('location: '.BASE_URL);
+      }
     }
   }
 
--- app/views/Admin/components/sidenav.php
+++ app/views/Admin/components/sidenav.php
@@ -1,3 +1,12 @@
 <div class="sticky-top sidenav bg-secondary">
-
+  <div class="sidenav-menu">
+    <div class="sidenav-item position-relative">
+      <img class="sidenav-logo" src="img/logo_ex_red.png" width="40" height="40"></img>
+      <p class="sidenav-text m-0">HMMMM</p>
+    </div>
+    <div class="sidenav-item position-relative">
+      <img class="sidenav-logo" src="img/logo_ex_red.png" width="40" height="40"></img>
+      <p class="sidenav-text m-0">HMMMM</p>
+    </div>
+  </div>
 </div>
