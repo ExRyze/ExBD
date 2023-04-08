345aae89eedb78eb7f45555e550e1c6b2879a41a
ExRyze <vaisyagovinandas@gmail.com>
2023-04-08 22:02:05 +0800

    Create Database & Update Mock-up

---
+++ database/migrations/2023_04_08_050559_create_animes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('type');
+            $table->integer('episodes');
+            $table->string('status');
+            $table->date('date_aired');
+            $table->date('date_finished');
+            $table->string('source');
+            $table->string('duration');
+            $table->string('rating');
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained("users")->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animes');
+    }
+};
